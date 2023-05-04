657d5998b27ba9e8c46d529854f06865975724a7
Mustafa Saadi <mustafa.saadi@toluna.com>
2023-05-04 12:48:06 +0300

    fixes & improvements - to be reviewed and fully tested before approval & merge

--- main.tf
+++ main.tf
@@ -1,5 +1,12 @@
 resource "aws_ecr_repository" "main" {
   name = var.repo_name
+  image_tag_mutability = "MUTABLE"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+  encryption_configuration {
+    encryption_type = "KMS"
+  }
 }
 
 resource "aws_ecr_lifecycle_policy" "main" {
